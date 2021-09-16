fce232bdc6055cbf0fa81759ff0d17b8ec1abf94
DAVEREX1 <65752189+daverex2k20@users.noreply.github.com>
2021-09-16 15:33:39 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string name;
+  int age;
+  cout<<"Enter Name"<<endl;
+  cin>>name;
+  cout<<"Welcome "<< name <<endl;
+  cout<<"How old are you "<< name <<"?"<<endl;
+  cin>>age;
+  cout<<"You are almost "<<age+1<<endl;
+}
+    
